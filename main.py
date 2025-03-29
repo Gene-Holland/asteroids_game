d1215e1683c8c3ec65de45ef2f85c9ed4ad424ec
Gene-Holland <geneholland1994@gmail.com>
2025-03-29 02:19:46 +0100

    B. small corrections

--- main.py
+++ main.py
@@ -4,20 +4,21 @@
 import pygame #importing pygame
 from constants import * #importing the constent so you get it too
 
-pygame.init()
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return  
+        screen.fill("black")    
+        pygame.display.flip()
 
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return  #it says this is out of function but it isn't
-    pygame.display.fill(000000)    
-    pygame.display.flip()
 print ("Starting Asteroids!")
 print (f"Screen width: {SCREEN_WIDTH}")
 print (f"Screen height: {SCREEN_HEIGHT}")
-if __name__ == "__name__":
+if __name__ == "__main__":
     main()
