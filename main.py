65405ed665a14074e7d8f27b279f3a384b9b4860
Gene-Holland <geneholland1994@gmail.com>
2025-03-29 23:45:55 +0100

    D. aligning with solutions

--- main.py
+++ main.py
@@ -2,33 +2,29 @@
 # the open-source pygame library
 # throughout this file
 import pygame #importing pygame
-from constants import * #importing the constent so you get it too
+from constants import *
+from player import Player
+
 
 def main():
     pygame.init()
-
-    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    timer = 60
-
-
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
-    
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  
-    
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return  
-        screen.fill("black")    
-        pygame.display.flip()
-        dt = (clock.tick(timer))/1000
-        
+                return
 
-        
-        
-print ("Starting Asteroids!")
-print (f"Screen width: {SCREEN_WIDTH}")
-print (f"Screen height: {SCREEN_HEIGHT}")
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
+
 if __name__ == "__main__":
     main()
